0bbd4f645114eb0f4052d0199d39b972ffd51a92
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-06-25 18:22:02 +0300

    Level1-Task03

---
+++ src/s02/Level1.java
@@ -0,0 +1,58 @@
+package s02;
+
+public class Level1 {
+
+    public static boolean findPointNull(int[][] fields) {
+        for (int[] a : fields) {
+            for (int x : a) {
+                if (x == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int[][] value(int[][] field, int x, int y, int day) {
+        if (0 <= x && x < field.length && 0 <= y && y < field[0].length && field[x][y] == 0) {
+            field[x][y] = day;
+        }
+        return field;
+    }
+
+
+    public static int ConquestCampaign(int N, int M, int L, int[] battalion) {
+        int counterDays = 0;
+        int[][] field = new int[N][M];
+
+        for (int i = 0; i < battalion.length - 1; i += 2) {
+            int x = battalion[i];
+            int y = battalion[i + 1];
+            field[x - 1][y - 1] = -1;
+        }
+
+        boolean flag = false;
+        while (!flag) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (field[i][j] == -1) {
+                        value(field, i - 1, j, counterDays + 1);
+                        value(field, i + 1, j, counterDays + 1);
+                        value(field, i, j - 1, counterDays + 1);
+                        value(field, i, j + 1, counterDays + 1);
+                    } else if (field[i][j] == counterDays - 1) {
+                        value(field, i - 1, j, counterDays);
+                        value(field, i + 1, j, counterDays);
+                        value(field, i, j - 1, counterDays);
+                        value(field, i, j + 1, counterDays);
+                    }
+                }
+            }
+            flag = findPointNull(field);
+            counterDays++;
+        }
+
+        return counterDays;
+    }
+
+}
---
+++ src/s02/Task01.java
@@ -0,0 +1,9 @@
+package s02;
+
+public class Task01 {
+    public static void main(String[] args) {
+        int N = 3, M = 4, L = 2;
+        int[] battalion = {2, 2, 3, 4};
+        System.out.println(Level1.ConquestCampaign(N, M, L, battalion));
+    }
+}
