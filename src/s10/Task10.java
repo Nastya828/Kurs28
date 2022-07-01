d22f8a2dc8b62a1bca2755af86f7ecd195199ea9
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-07-01 14:08:18 +0300

    Level1-Task10

---
+++ src/s10/Task10.java
@@ -0,0 +1,8 @@
+package s10;
+
+public class Task10 {
+    public static void main(String[] args) {
+        String line = "£iuyh&]";
+        System.out.println(Level1.PrintingCosts(line));
+    }
+}
