4ebb8f7e2e5e775cdd8d07df8a3f6e825c4d8ac8
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-07-11 22:04:50 +0300

    Level1-Task24

---
+++ src/s24/Level1.java
@@ -0,0 +1,64 @@
+package s24;
+
+import java.util.Arrays;
+
+public class Level1 {
+
+    public static boolean Football(int[] F, int N) {
+
+        int[] f1 = Arrays.copyOf(F, F.length);
+
+        for (int i = 0; i < F.length; i++) {
+            for (int j = 0; j < F.length; j++) {
+                if (i != j) {
+                    f1 = funcFirst(F, i, j);
+                }
+                if (order(f1)) {
+                    return true;
+                }
+            }
+        }
+
+        int k;
+        for (int i = 1; i <= F.length - 1; i++) {
+            for (int j = 0; j <= (F.length - i - 1); j++) {
+                k = i + j;
+                int[] f2 = funcSecond(F, j, k);
+                if (order(f2)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean order(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] funcFirst(int[] F, int i, int j) {
+        int[] a = Arrays.copyOf(F, F.length);
+        int x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+        return a;
+    }
+
+    public static int[] funcSecond(int[] F, int i, int j) {
+        int[] a = Arrays.copyOf(F, F.length);
+
+        while (i < j && j > 0 && i < a.length) {
+            a = funcFirst(a, i, j);
+            i++;
+            j--;
+        }
+        return a;
+    }
+
+}
---
+++ src/s24/Task24.java
@@ -0,0 +1,24 @@
+package s24;
+
+public class Task24 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Level1.Football(new int[]{1, 3, 2}, 3));
+
+        System.out.println(Level1.Football(new int[]{3, 2, 1}, 3));
+
+        int[] x = {4, 3, 2, 1, 5};
+        System.out.println(Level1.Football(x, x.length));
+
+        int[] x1 = {1, 4, 3, 2, 5};
+        System.out.println(Level1.Football(x1, x1.length));
+
+        int[] x2 = {1, 7, 5, 3, 9};
+        System.out.println(Level1.Football(x2, x2.length));
+
+        int[] x3 = {1};
+        System.out.println(Level1.Football(x3, x3.length));
+
+    }
+}
